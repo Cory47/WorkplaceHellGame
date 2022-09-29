370e2e922c589c9c5e7258e6cfb79891a59149a5
Cory Marleau <corymarleau@Corys-MacBook-Pro.local>
2022-09-28 17:18:34 -0700

    Added coworkers

---
+++ Workplace_Hell_demo3_GUI1.0/Workplace_Hell/Scenes/levels/Path.gd
@@ -0,0 +1,14 @@
+extends Path2D
+
+var inc = 0;
+var speed = 250;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	inc += delta*speed
+	$PathFollow2D.offset = inc
